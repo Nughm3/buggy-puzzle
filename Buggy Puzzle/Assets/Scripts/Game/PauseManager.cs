d17ed068a7f1d98a9d26a2b41fe9c1e8fd8f0e74
troll530 <lauedmondyc@gmail.com>
2021-11-03 09:24:53 +0800

    added pause manager and menu scripts, need continue and quit button uis

---
+++ Buggy Puzzle/Assets/Scripts/Game/PauseManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    public static bool isPaused = false;
+    public GameObject pauseMenu;
+
+    void Update() {
+        UpdateSelection();
+        if (Input.GetKeyDown(KeyCode.Escape)) Pause();
+    }
+
+    void UpdateSelection() {
+        if (isPaused) {
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0f;
+        }
+        else {
+            pauseMenu.SetActive(false);
+            Time.timeScale = 1f;
+        }
+    }
+
+    void Pause() {
+        isPaused = !isPaused;
+    }
+}
---
+++ Buggy Puzzle/Assets/Scripts/Game/PauseMenu.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+using TMPro;
+
+public class PauseMenu : MonoBehaviour
+{
+    public TextMeshPro continueButton;
+    public TextMeshPro quitButton;
+
+    public GameObject menu;
+    public GameObject game;
+
+    int selectedOption = 0;
+    int options = 2;
+
+    void Awake() {
+        UpdateSelection();
+    }
+
+    public void UpdateSelection() {
+        TextMeshPro[] buttons = {continueButton, quitButton};
+
+        foreach (TextMeshPro button in buttons) {
+            button.color = Color.black;
+            button.fontStyle = FontStyles.Normal;
+        }
+        buttons[selectedOption].color = Color.green;
+        buttons[selectedOption].fontStyle = FontStyles.Bold;
+    }
+
+    void SelectUp() {
+        if (selectedOption > 0) selectedOption--;
+        else selectedOption = options - 1;
+    }
+
+    void SelectDown() {
+        if (selectedOption < options - 1) selectedOption++;
+        else selectedOption = 0;
+    }
+
+    void Confirm() {
+        switch(selectedOption) {
+            case 0:
+                Continue();
+                return;
+            case 1:
+                Quit();
+                return;
+        }
+    }
+
+    void Unconfirm() {
+        PauseManager.isPaused = false;
+    }
+
+    void Continue() {
+        PauseManager.isPaused = false;
+    }
+
+    void Quit() {
+        selectedOption = 0;
+        PauseManager.isPaused = false;
+        Time.timeScale = 1f;
+        menu.SetActive(true);
+        game.SetActive(false);
+        gameObject.SetActive(false);
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) SelectUp();
+        else if (Input.GetKeyDown(KeyCode.RightArrow)) SelectDown();
+        if (Input.GetKeyDown(KeyCode.Z)) Confirm();
+        else if (Input.GetKeyDown(KeyCode.X)) Unconfirm();
+        UpdateSelection();
+    }
+}
